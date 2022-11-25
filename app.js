3f02d5a9c7eb69b54ef96ab6f5af0cc7efa0f2ed
ArielDigital <arielmosquera@gmail.com>
2022-11-24 21:00:46 -0300

    config  puerto servidor

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const path = require("path");
 
 app.use(express.static("public"));
 
-app.listen(3001, () => console.log("Servidor corriendo en el 3001"));
+app.listen(process.env.PORT || 3001, () => console.log("Servidor corriendo en el 3001"));
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("./views/home.html"));
